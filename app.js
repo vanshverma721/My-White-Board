8f9c1dfc6ea635cc8305f2403992291ce273a4ca
vanshverma721 <vvv.721781@gmail.com>
2022-12-26 14:38:05 +0530

    my project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+
+app.use(express.static("public"));
+
+let port = 4321;
+let server = app.listen(port, () => {
+    console.log("Listening to port" + port);
+})
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+    
+    socket.on("beginPath", (data) => {
+        io.sockets.emit("beginPath", data);
+    })
+
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
+})
